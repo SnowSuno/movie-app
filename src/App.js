32b624295f5ec641a04cd9f3bbb189944ddfaf61
SnowSuno <percy3368@gmail.com>
2021-06-25 19:11:22 +0900

    feat : add Movie component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import Movie from "./Movie";
 
 class App extends React.Component {
     state = {
@@ -12,19 +13,38 @@ class App extends React.Component {
             data: {
                 data: { movies }
             }
-        } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+        } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
         this.setState({ movies, isLoading: false });
+    };
+    renderMovies = () => {
+        /**
+         * @param movie.id
+         * @param movie.year
+         * @param movie.title
+         * @param movie.summary
+         * @param movie.medium_cover_image
+         */
+        const { movies } = this.state;
+        return movies.map(movie => <Movie
+            key={movie.id}
+            id={movie.id}
+            year={movie.year}
+            title={movie.title}
+            summary={movie.summary}
+            poster={movie.medium_cover_image}
+        />);
     };
 
     componentDidMount() {
         this.getMovies().then();
     };
-
     render() {
         const { isLoading } = this.state;
         return (
             <div>
-                {isLoading ? "Loading" : "We are ready"+this.state.movies}
+                {isLoading
+                    ? "Loading"
+                    : this.renderMovies()}
             </div>
         );
     };
--- src/Movie.js
+++ src/Movie.js
@@ -1,11 +1,18 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-
-class Movie extends React.Component {
-
-
-
-    render() {
-
-    }
+function Movie({id, year, title, summary, poster}) {
+    return (
+        <h5>{title}</h5>
+    );
 }
+
+Movie.propTypes = {
+    id: PropTypes.number.isRequired,
+    year: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    summary: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired
+};
+
+export default Movie;
